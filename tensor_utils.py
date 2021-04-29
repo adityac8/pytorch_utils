74efaa92101afd5f77044733f8e6e92d20999394
Aditya Arora <adityadvlp@gmail.com>
2021-04-29 16:08:20 +0400

    Channel Unshuffle

--- tensor_utils.py
+++ tensor_utils.py
@@ -2,6 +2,12 @@ import torch
 
 # Channel Shuffle
 def channel_shuffle(x):
-    batchsize, num_channels, height, width = x.data.size()
+    batchsize, num_channels, height, width = x.size()
     assert (num_channels % 4 == 0)
     return torch.cat((x[:,::2,:,:],x[:,1::2,:,:]),1)
+
+# Channel Unshuffle
+def channel_unshuffle(x):
+    batchsize, num_channels, height, width = x.size()
+    assert (num_channels % 4 == 0)
+    return x.view(batchsize, 2, num_channels//2, height, width).permute(0,2,1,3,4).reshape(batchsize, num_channels, height, width)
